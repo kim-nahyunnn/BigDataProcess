864f106c03344e64063ce87c936b379f4f192b41
kim-nahyunnn <nahyun8701@gmail.com>
2022-10-31 18:08:59 +0900

    add IMBDStudent20200945 code

--- HW3/IMDBStudent20200945.py
+++ HW3/IMDBStudent20200945.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 
-print("hello")
+import sys
+
+rslt = dict()
+
+with open(sys.argv[1], "rt") as f:
+	for line in f:
+		array = line.split("::")
+		rsltArray = array[2].split("|")
+		for i in rsltArray:
+			if i.strip() not in rslt:
+				rslt[i.strip()] = 1
+			else:
+				rslt[i.strip()] += 1
+
+with open(sys.argv[2], "wt") as f:
+	for key in rslt.keys():
+		f.write(key)
+		f.write(" ")
+		f.write(str(rslt[key]))
+		f.write("\n")
